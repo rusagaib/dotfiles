7835768337fdb0a0804cd706c6ef88712cafcaa5
rusagaib <r.dewangga.ra@gmail.com>
2023-04-22 21:43:01 +0700

    fixing tmux.conf colorscheme & adding new feat: prodide.sh

--- tmux/prodide.sh
+++ tmux/prodide.sh
@@ -14,13 +14,33 @@ function makeitrun ()
       tmux new-window -t prod
       tmux rename-window -t prod:3 'term' 
       tmux new-window -t prod
-      tmux rename-window -t prod:4 'lazygit'
+      tmux rename-window -t prod:4 'mocp'
       tmux new-window -t prod
       tmux rename-window -t prod:5 'htop'
       tmux send-keys -t prod:htop "htop" C-m     
     ;;
+    a|attach)
+      tmux a -t prod 
+    ;;
+    q|quit)
+      echo -e "Killing all tmux session..."
+      sleep 2 
+      tmux kill-session -t prod 
+    ;;
+    qa|quit)
+      echo -e "Killing all tmux session..."
+      sleep 2 
+      tmux kill-server  
+    ;;
+    -h|--help)
+      echo -e "i|s|a|l|q| - init|start|attach|list|quit/kill-session"
+    ;;
+    l|ls|list)
+      echo -e "current session: "
+      tmux ls
+    ;;
     *) 
-      echo "no opts.." 
+      echo "no opts.. (or read -h)" 
     ;;
   esac
 }
